c30c6c5ef1afd8d7072b3c2a90fe7600217777b2
Derek D <xlcrdev@users.noreply.github.com>
2026-07-20 20:13:57 -0400

    More misc fixes of syntax.

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,19 @@
 <template>
   <div class="grid grid-cols-4">
     <div class="post" v-for="post in posts" :key="post.id">
-      
       <div class="p-4">
         <div class="max-w-sm rounded shadow-lg mb-4">
           <img
             class=""
             :src="post.featured_image_src_large[0]"
             alt="" 
-            height=""
-          >
+            height="">
           <div class="px-6 py-4 pb-2 bg-gray-300">
             <div class="font-bold text-lg mb-2"><a :href="`blog/${post.slug}`" v-html="post.title.rendered"></a></div>
             <div class="text-xs text-green-500 mb2">{{ post.date }}</div>
-            <div class="text-black text-base line-clamp-3>
-              <div v-html="post.excerpt.rendered"></div>
+            <div 
+            class="text-black text-base line-clamp-3>
+              <div v-html=post.excerpt.rendered></div>
             </div>
           </div>
         </div>
@@ -26,13 +25,13 @@
 
 <script>
   export default {
-    computed: {
-      posts() { 
-        return this.$store.state.posts;
-      },
+  computed: {
+    posts() { 
+      return this.$store.state.posts;
     },
-    created() {
-      this.$store.dispatch("getPosts");
-    },
-  };
+  },
+  created() {
+    this.$store.dispatch(getPosts);
+  },
+};
 </script>
